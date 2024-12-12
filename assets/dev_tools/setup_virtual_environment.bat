1a36ace95db26e348ec3b41e7dbb167697586619
Mythical-Github <MythicalData@gmail.com>
2024-12-11 22:09:46 -0500

    Updated dev tools

--- assets/dev_tools/setup_virtual_environment.bat
+++ assets/dev_tools/setup_virtual_environment.bat
@@ -2,26 +2,11 @@
 
 cd /d %~dp0
 
-set "py_project_dev_tools_exe=%CD%/py_project_dev_tools.exe"
+set "py_project_dev_tools_exe=%CD%\py_project_dev_tools.exe"
 
-set "project_info_ini=%CD%/project_info.ini"
+set "toml=%CD%\..\..\pyproject.toml"
 
-set "repo_url="
-set "repo_name="
-
-for /f "usebackq tokens=1,* delims==" %%A in ("%project_info_ini%") do (
-    if "%%A"=="repo_url" set "repo_url=%%B"
-)
-
-if not defined repo_url echo Missing repo_url in config file && exit /b 1
-
-for %%A in (%repo_url%) do set "repo_name=%%~nA"
-
-if not defined repo_name echo Missing repo_name in config file && exit /b 1
-
-set "toml=%CD%/%repo_name%/pyproject.toml"
-
-"%py_project_dev_tools_exe%" setup_virtual_environment "%toml%"
+"%py_project_dev_tools_exe%" setup_virtual_environment --project_toml_path "%toml%"
 
 exit /b 0
 
